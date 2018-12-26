04cbde3832b4cee9a5c304faceb593f3ea756e54
王军 <>
2018-12-26 19:58:26 +0800

    first commit

---
+++ src/main/java/com/spring/es/springes/SpringEsApplication.java
@@ -0,0 +1,14 @@
+package com.spring.es.springes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEsApplication.class, args);
+	}
+
+}
+
